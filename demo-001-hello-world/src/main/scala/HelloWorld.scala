69ff048a70c78b655a6e8e536ab75e3d4b6bc250
heyiyang <heyiyang@linespace.cn>
2024-11-08 16:32:08 +0800

    Initial commit

--- demo-001-hello-world/src/main/scala/HelloWorld.scala
+++ demo-001-hello-world/src/main/scala/HelloWorld.scala
@@ -1,3 +1,9 @@
-object HelloWorld {
+import geotrellis.raster._
 
+import scala.util.Success
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    val error = new GeoAttrsError("Hello, world!")
+    println(error)
+  }
 }
