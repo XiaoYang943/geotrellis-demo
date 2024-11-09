f6aa3177bdcc9d392e1ed6f3586f3ccb213ff472
heyiyang <heyiyang@linespace.cn>
2024-11-09 14:10:31 +0800

    feat: ReadSingleBandTifByFileSystem

---
+++ demo-010-ingest-raster-layer/src/main/scala/ReadSingleBandTifByFileSystem.scala
@@ -0,0 +1,12 @@
+
+import geotrellis.raster.io.geotiff.reader.GeoTiffReader
+import geotrellis.raster.io.geotiff.{GeoTiffReader, SinglebandGeoTiff}
+
+object ReadSingleBandTifByFileSystem {
+  def main(args: Array[String]): Unit = {
+    val path: String = "D:\\data\\raster\\geotiff\\dem12.5m\\陕西\\陕西.tif"
+    val geoTiff: SinglebandGeoTiff = GeoTiffReader.readSingleband(path)
+    print(geoTiff.raster.rasterExtent)
+  }
+
+}
